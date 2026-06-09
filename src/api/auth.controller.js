7b9aa6a049cb0e7602b728769ceeae8cdfb9f452
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-06-09 21:28:03 +0300

    Hash password on registration and exclude it from response

--- src/api/auth.controller.js
+++ src/api/auth.controller.js
@@ -1,10 +1,18 @@
 'use strict';
 
+const bcrypt = require('bcrypt');
 const { usersRepository } = require('../entity/users.repository.js');
+
+const SALT_ROUNDS = 10;
 
 const register = async (req, res) => {
   const { email, password } = req.body;
-  const user = await usersRepository.create(email, password);
+
+  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+  const { password: _, ...user } = await usersRepository.create(
+    email,
+    hashedPassword,
+  );
 
   res.json({ user });
 };
