bbd08fc31a1793bd49f78c6ee35de935a7cef52b
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-06-10 13:40:27 +0300

    Add mailer utility for sending transactional emails via Gmail

---
+++ src/utils/mailer.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+function send(email, subject, html) {
+  return transporter.sendMail({
+    from: 'Auth API',
+    to: email,
+    subject,
+    html,
+  });
+}
+
+const mailer = { send };
+
+module.exports = { mailer };
