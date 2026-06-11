e584c8af1b8e1555787cf92642b47ac2a2fd5a37
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-06-11 15:40:39 +0300

    Add refresh token generation and validation with separate secret

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -3,6 +3,7 @@
 const jsonwebtoken = require('jsonwebtoken');
 
 const SECRET = process.env.JWT_SECRET;
+const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
 
 function generateAccessToken(user) {
   return jsonwebtoken.sign(user, SECRET, { expiresIn: '10m' });
@@ -16,9 +17,23 @@ function validateAccessToken(token) {
   }
 }
 
+function generateRefreshToken(user) {
+  return jsonwebtoken.sign(user, REFRESH_SECRET, { expiresIn: '7d' });
+}
+
+function validateRefreshToken(token) {
+  try {
+    return jsonwebtoken.verify(token, REFRESH_SECRET);
+  } catch (error) {
+    return null;
+  }
+}
+
 const jwt = {
   generateAccessToken,
   validateAccessToken,
+  generateRefreshToken,
+  validateRefreshToken,
 };
 
 module.exports = { jwt };
